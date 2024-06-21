f746cc102233fa80cae2d7dd7d59ab87948abaae
Arghya Das <arghyadasproject@gmail.com>
2024-06-22 00:24:02 +0530

    embla-carousel with responsiveness

--- src/components/glowcontainer.tsx
+++ src/components/glowcontainer.tsx
@@ -13,23 +13,23 @@ const GlowContainer = forwardRef(function GlowContainer(
       ref={ref}
       className={clsx(
         className,
-        "relative @container before:absolute before:inset-3 before:-z-20 before:rounded-full before:bg-purple-500/50 before:blur-2xl lg:before:inset-4 lg:before:blur-3xl"
+        "glowcontainer relative before:absolute before:inset-3 before:-z-20 before:rounded-full before:bg-purple-500/50 before:blur-2xl lg:before:inset-4 lg:before:blur-3xl"
       )}
     >
       <div
         ref={ref}
         className={clsx(
           className,
-          "group relative z-0 overflow-hidden rounded-3xl p-[1px] ring-1 ring-purple-300/15"
+          "group relative z-0 overflow-hidden rounded-xl p-[1px] ring-1 ring-purple-300/15"
         )}
       >
         <div
           className={clsx(
-            "absolute left-1/2 top-1/2 -z-10 aspect-square w-[200%] origin-top-left animate-[rotateFade_3500ms_cubic-bezier(1,0,0,1)_infinite] bg-[conic-gradient(var(--tw-gradient-stops))] from-transparent via-transparent to-purple-500 opacity-100 ease-linear"
+            "absolute left-1/2 top-1/2 -z-10 aspect-square w-[200%] origin-top-left animate-[rotateFade_3500ms_cubic-bezier(1,0,0,1)_infinite] bg-[conic-gradient(var(--tw-gradient-stops))] from-transparent via-transparent to-purple-500 opacity-100 ease-linear "
           )}
         />
-        <div className={clsx("rounded-3xl bg-neutral-900 p-2 sm:p-3 h-full")}>
-          <div className="overflow-hidden rounded-2xl h-full">{children}</div>
+        <div className={clsx("rounded-xl bg-neutral-900 p-2 sm:p-3 h-full")}>
+          <div className="overflow-hidden rounded-xl h-full">{children}</div>
         </div>
       </div>
     </div>
